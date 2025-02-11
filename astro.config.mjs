c4cc8811c6aa9ca9d088dae19efa2b8fd8f255fc
Niklas P <contact@niklasplessing.net>
2025-02-11 14:49:20 +0100

    routing issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind({ applyBaseStyles: false })],
-    site: "https://niklasp.github.io/",
-    base: "/MehrVomLohn.de/",
+    site: "https://niklasp.github.io",
+    base: "/MehrVomLohn.de",
 });
